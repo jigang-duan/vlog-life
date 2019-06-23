62a4e64f34c481d821d5710156dd94f0a2d43848
jigang.duan <djg4055108@126.com>
2019-06-23 23:14:52 +0800

    modify about

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -12,6 +12,27 @@ App({
       })
     }
 
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+
     // 获取系统状态栏信息
     wx.getSystemInfo({
       success: e => {
@@ -31,7 +52,8 @@ App({
   },
   globalData: {
     hasLogin: false,
-    openid: null
+    openid: null,
+    userInfo: null
   },
   // lazy loading openid
   getUserOpenId(callback) {
@@ -74,9 +96,5 @@ App({
       this.globalData.openid = res.result.openid
       return res.result.openid
     })
-  },
-
-  globalData: {
-    userInfo: null
   }
 })
